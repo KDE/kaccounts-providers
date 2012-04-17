3d3940c5c503cb4dba81a924c27aef005d6fd315
Alex Fiestas <afiestas@kde.org>
2012-04-17 16:29:11 +0200

    ownCloud setup wizard with credentials page implemented (BasicInfo)
    In this case we are calling the credential page "Basic Info" since
    it is used to get information about the ownCloud server itself.

---
+++ owncloud.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************************
+ *  Copyright (C) 2012 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include "owncloud.h"
+#include "basicinfo.h"
+
+#include <klocalizedstring.h>
+#include <kpushbutton.h>
+#include <kstandardguiitem.h>
+
+OwnCloudWizard::OwnCloudWizard(QWidget* parent, Qt::WindowFlags flags): QWizard(parent, flags)
+{
+    BasicInfo *basicInfo = new BasicInfo(this);
+
+    addPage(basicInfo);
+
+    setButton(QWizard::BackButton, new KPushButton(KStandardGuiItem::back(KStandardGuiItem::UseRTL)));
+    setButton(QWizard::NextButton, new KPushButton(KStandardGuiItem::forward(KStandardGuiItem::UseRTL)));
+    setButton(QWizard::FinishButton, new KPushButton(KStandardGuiItem::apply()));
+    setButton(QWizard::CancelButton, new KPushButton(KStandardGuiItem::cancel()));
+
+    //We do not want "Forward" as text
+    setButtonText(QWizard::NextButton, i18nc("Action to go to the next page on the wizard", "Next"));
+    setButtonText(QWizard::FinishButton, i18nc("Action to finish the wizard", "Finish"));
+}
+
+OwnCloudWizard::~OwnCloudWizard()
+{
+
+}
