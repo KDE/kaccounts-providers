919ec58aa591d08237d77d8c09d9ce6ed2924b6b
Àlex Fiestas <afiestas@kde.org>
2013-02-19 18:56:29 +0100

    First import of our providers and services, atm twitter and identi.ca
    Since we'll be using our providers and services we won't be cross
    platform from day one, but this is something we'll fix with upstream
    after we experiment a little bit with our own.
    
    Upstream wants to unify this, but all pieces are not there yet.
    
    We'll start by porting choqok so we can learn more about accounts-sso
    and what we'll be the best way to make app's really cross-platform.

---
+++ cmake/modules/FindAccountsFileDir.cmake
@@ -0,0 +1,29 @@
+find_package(PkgConfig)
+
+find_package(PkgConfig REQUIRED)
+
+execute_process(
+    COMMAND "${PKG_CONFIG_EXECUTABLE}" --variable=providerfilesdir libaccounts-glib
+    OUTPUT_VARIABLE _pkgconfig_invoke_result
+    RESULT_VARIABLE _pkgconfig_failed)
+
+if (_pkgconfig_failed)
+    message(FAILED "couldn-t find the providerfilesdir for in libaccounts-glib")
+else()
+    string(REGEX REPLACE "[\r\n]"                  " " _pkgconfig_invoke_result "${_pkgconfig_invoke_result}")
+    string(REGEX REPLACE " +$"                     ""  _pkgconfig_invoke_result "${_pkgconfig_invoke_result}")
+    set(ACCOUNTS_PROVIDERS_DIR ${_pkgconfig_invoke_result})
+endif()
+
+execute_process(
+    COMMAND "${PKG_CONFIG_EXECUTABLE}" --variable=servicefilesdir libaccounts-glib
+    OUTPUT_VARIABLE _pkgconfig_invoke_result
+    RESULT_VARIABLE _pkgconfig_failed)
+
+if (_pkgconfig_failed)
+    message(FAILED "couldn-t find the providerfilesdir for in libaccounts-glib")
+else()
+        string(REGEX REPLACE "[\r\n]"                  " " _pkgconfig_invoke_result "${_pkgconfig_invoke_result}")
+        string(REGEX REPLACE " +$"                     ""  _pkgconfig_invoke_result "${_pkgconfig_invoke_result}")
+        set(ACCOUNTS_SERVICES_DIR ${_pkgconfig_invoke_result})
+endif()
