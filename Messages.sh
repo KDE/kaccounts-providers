f44c451baf687eab66bf4ec74e269659511614b4
Martin Klapetek <mklapetek@kde.org>
2014-10-29 10:31:08 +0100

    The provider/service files now have .in suffix, fix the Messages.sh lookup

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-find . -name "*.provider" -o -name "*.service" -type f | xargs -n1 intltool-extract -l -type="gettext/xml"
+find . -name "*.provider.in" -o -name "*.service.in" -type f | xargs -n1 intltool-extract -l -type="gettext/xml"
 $XGETTEXT `find . -name "*.h"` -c -a -o $podir/kaccounts-providers.pot
 rm -rf ./tmp/
