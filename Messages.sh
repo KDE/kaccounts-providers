08b79556b2423517c1052ddbd55b698e32ff389c
Martin Klapetek <mklapetek@kde.org>
2015-10-15 21:25:07 -0400

    Extend Messages.sh to also cover .cpp files

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 find . -name "*.provider.in" -o -name "*.service.in" -type f | xargs -n1 intltool-extract -l -type="gettext/xml"
-$XGETTEXT `find . -name "*.h"` -c -a -o $podir/kaccounts-providers.pot
+$XGETTEXT `find . -name "*.h" -o -name "*.cpp"` -c -a -o $podir/kaccounts-providers.pot
 rm -rf ./tmp/
