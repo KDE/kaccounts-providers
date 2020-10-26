9f6bd24889850aa3f65d0b64fcf6bbe733f95dd4
Nicolas Fella <nicolas.fella@gmx.de>
2020-10-27 00:45:09 +0100

    Extract messages from QML files
    BUG: 388203

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 find . -name "*.provider.in" -o -name "*.service.in" -type f | xargs -n1 intltool-extract -l -type="gettext/xml"
-$XGETTEXT `find . -name "*.h" -o -name "*.cpp"` -c -kN_ -kC_:1c,2 -o $podir/kaccounts-providers.pot
+$XGETTEXT `find . -name "*.h" -o -name "*.cpp" -o -name "*.qml"` -c -kN_ -kC_:1c,2 -o $podir/kaccounts-providers.pot
 rm -rf ./tmp/
